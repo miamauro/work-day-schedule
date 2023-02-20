1851595e61bcc572377ef449e68df5a01c5729cc
miamauro <miiamauro@outlook.com>
2023-02-19 20:53:48 -0600

    add function to save tasks to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,29 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-var timeBlock = $(".time-block");
-var saveButton = $(".saveBtn");
-
 $(function () {
+  var timeBlock = $(".time-block");
+  var saveButton = $(".saveBtn");
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+  saveButton.on("click", function () {
+    var taskInput = $(this).siblings(".description").val();
+    const hour = $(this).parent().attr("id");
+    console.log(hour);
+    console.log(taskInput);
+    var newTask = {
+      hour: hour,
+      taskInput: taskInput,
+    };
+    var savedTasks = JSON.parse(localStorage.getItem("savedTasks")) || [];
+    savedTasks.push(newTask);
+    localStorage.setItem("savedTasks", JSON.stringify(savedTasks));
+  });
 
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -53,5 +66,5 @@ $(function () {
   }
 
   displayDay();
-  setInterval(displayDay, 60000);
+  setInterval(displayDay, 1000);
 });
